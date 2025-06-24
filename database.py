f921e58343fa36d4d79d90f6ac1c0ff80383bc0b
Hemanth Nagaraj <mrhemanthn@gmail.com>
2025-06-24 13:15:30 +0200

    database.py

--- database.py
+++ database.py
@@ -10,6 +10,8 @@ WHERE name = ?
 ORDER BY rating DESC
 LIMIT 1;"""
 
+GET_METHODS_TO_RATINGS = "SELECT method, AVG(rating) FROM beans GROUP BY method;"
+
 def connect():
     return sqlite3.connect("data.db")
 
@@ -32,5 +34,9 @@ def get_beans_by_name(connection, name):
 def get_best_preparation_for_bean(connection, name):
     with connection:
         return connection.execute(GET_BEST_PREPARATION_FOR_BEAN, (name,)).fetchone()
+
+def get_methods_to_ratings(connection):
+    with connection:
+        return connection.execute(GET_METHODS_TO_RATINGS).fetchall()
 
 
